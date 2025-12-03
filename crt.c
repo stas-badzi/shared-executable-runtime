5ac27fe394dad5fbdda29b9e952cf88f68466388
Stanisław Badziak <151390437+stas-badzi@users.noreply.github.com>
2025-12-03 21:30:12 +0100

    fix part 1

--- crt.c
+++ crt.c
@@ -4,8 +4,6 @@ extern void _exit (int __status) __attribute__ ((__noreturn__));
 int main();
 
 void _start() {
-#ifdef __ANDROID__ // fix android libc bug/undefined behaviour
     __asm__("andq $-0x10,%rsp");
-#endif
     _exit(main());
 }
