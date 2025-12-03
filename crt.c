b95f8b1fe45b0aea158133d569c4dfc215d84252
stas-badzi <sss.badziak@gmail.com>
2025-12-03 23:18:51 +0100

    other arch assembly

--- crt.c
+++ crt.c
@@ -4,6 +4,12 @@ extern void _exit (int __status) __attribute__ ((__noreturn__));
 int main();
 
 void _start() {
+#ifdef __x86_64__
     __asm__("andq $-0x10,%rsp");
+#elif __i386__
+    __asm__("andl $-0x10,%esp");
+#else // ARM
+   __asm__("eor sp, sp, #-0x10");
+#endif
     _exit(main());
 }
