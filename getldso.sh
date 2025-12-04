2d89482dbfe8bd92ee1a40a751316ed9a251d653
Stanisław Badziak <151390437+stas-badzi@users.noreply.github.com>
2025-12-04 20:57:26 +0100

    fix macos

--- getldso.sh
+++ getldso.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 echo "int main() {}" > tmp.c
+export readelf=readelf
+(uname -s | grep Darwin 2>/dev/null >/dev/null) && export readelf=greadelf
+
 if [ -z $1 ]
 then
   export CC=cc
@@ -7,5 +10,5 @@ else
   export CC=$1
 fi
 $CC tmp.c -o tmp.out $CFLAGS || exit 1
-readelf -Wl tmp.out | \
+$readelf -Wl tmp.out | \
   sed -n 's/.*interpreter: \(.*\)]/\1/p'
